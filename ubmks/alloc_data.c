2c042e8dc82ace7385b84433e0188dde21434b94
Bijan Tabatabai <btabatabai@wisc.edu>
2025-11-25 15:17:41 +0000

    alloc_data: Print out the time taken to allocate

--- ubmks/alloc_data.c
+++ ubmks/alloc_data.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
+    struct timeval stop, start;
     size_t size;
+    time_t alloc_time_usec;
+    time_t alloc_time_sec;
+    time_t alloc_ms_remainder;
     size_t pagesize = sysconf(_SC_PAGESIZE);
 
     if (argc != 2) {
@@ -13,6 +18,7 @@ int main(int argc, char *argv[]) {
     }
     size = (size_t)atoll(argv[1]) * 1024 * 1024 * 1024;
 
+    gettimeofday(&start, NULL);
     void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
     if (ptr == MAP_FAILED) {
 	perror("mmap failed");
@@ -24,9 +30,16 @@ int main(int argc, char *argv[]) {
     for (size_t offset = 0; offset < size / sizeof(long); offset += pagesize / sizeof(long)) {
 	((long *)ptr)[offset] = (long)(offset);
     }
+    gettimeofday(&stop, NULL);
+
+    alloc_time_usec = (stop.tv_sec - start.tv_sec) * 1000000 +
+        (stop.tv_usec - start.tv_usec);
+    alloc_time_sec = alloc_time_usec / 1000000;
+    alloc_ms_remainder = (alloc_time_usec % 1000000) / 1000;
 
     // Keep the memory allocated for a while to allow inspection
-    printf("Allocated %zu bytes of memory. Press Enter to free and exit...\n", size);
+    printf("Allocated %zu bytes of memory. In %ld.%ld seconds\n", size,
+        alloc_time_sec, alloc_ms_remainder);
     getchar();
 
     return 0;
