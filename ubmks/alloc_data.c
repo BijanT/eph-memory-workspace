ff57f453039e5a33aa23e52219f42680011f6a3c
Bijan Tabatabai <btabatabai@wisc.edu>
2026-01-14 14:54:33 -0600

    alloc_data: Add option to end after the data is allocated

--- ubmks/alloc_data.c
+++ ubmks/alloc_data.c
@@ -3,6 +3,7 @@
 #include <sys/mman.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
     struct timeval stop, start;
@@ -11,12 +12,16 @@ int main(int argc, char *argv[]) {
     time_t alloc_time_sec;
     time_t alloc_ms_remainder;
     size_t pagesize = sysconf(_SC_PAGESIZE);
+    bool wait = true;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <size in GB>\n", argv[0]);
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "Usage: %s <size in GB> [don't wait]\n", argv[0]);
         return -1;
     }
     size = (size_t)atoll(argv[1]) * 1024 * 1024 * 1024;
+    if (argc == 3) {
+        wait = false;
+    }
 
     gettimeofday(&start, NULL);
     void *ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
@@ -41,7 +46,9 @@ int main(int argc, char *argv[]) {
     printf("Allocated %zu bytes of memory. In %ld.%ld seconds\n", size,
         alloc_time_sec, alloc_ms_remainder);
     fflush(stdout);
-    getchar();
+    if (wait) {
+        getchar();
+    }
 
     return 0;
 }
